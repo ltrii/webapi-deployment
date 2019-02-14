88dd13dc5d6a36aad69bdf187e599829bbd03f12
ltrii <lelandtr@gmail.com>
2019-02-14 10:38:39 -0800

    For deployment

--- data/db.js
+++ data/db.js
@@ -1,5 +1,5 @@
 const knex = require('knex');
 
-const config = require('../knexfile.js').development;
+const config = require('../knexfile.js')[process.env.DBENV];
 
 module.exports = knex(config);
